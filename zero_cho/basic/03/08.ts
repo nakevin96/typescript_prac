3279d16265a03b8a78f2ff5d5128c97e27826aea
nakevin96 <nakevin96@hanyang.ac.kr>
2023-07-14 21:31:18 +0900

    Study: filter 제네릭 분석

--- zero_cho/basic/03/08.ts
+++ zero_cho/basic/03/08.ts
@@ -26,4 +26,17 @@ interface Array<T> {
 // 이로 인해 stringArr의 타입이 string[]로 잘 추론되는 것이다.
 const stringArr = [1, 2, 3].map((item) => item.toString());
 
+//  filter<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S[];
+//  filter(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): T[];
+const filteredArr = [1, 2, 3, 4, 5].filter((item) => item % 2 === 0);
+const filteredArr2 = [1, "2", 3, "4", 5].filter(
+  (item) => typeof item === "string"
+);
+
+const filterPredicate = (value: string | number): value is string =>
+  typeof value === "string";
+const filteredArr3 = [1, "2", 3, "4", 5].filter(filterPredicate);
+const filteredArr4 = [1, "2", 3, "4", 5].filter(
+  (value: string | number): value is string => typeof value === "string"
+);
 export {};
