05cf4b139b91262a0deac0884be33b68372e8e0d
nakevin96 <nakevin96@hanyang.ac.kr>
2023-07-15 18:31:09 +0900

    Study: filter 함수 타입 만들어보기

--- zero_cho/basic/03/09.ts
+++ zero_cho/basic/03/09.ts
@@ -2,6 +2,8 @@
 interface Arr<T> {
   forEach(callbackfn: (item: T) => void): void;
   map<U>(callbackfn: (item: T, index: number, array: T[]) => U): U[];
+  filter<S extends T>(callbackfn: (item: T) => item is S): S[];
+  filter(callbackfn: (item: T) => unknown): T[];
 }
 
 const a: Arr<number> = [1, 2, 3];
@@ -32,3 +34,15 @@ const aMap2 = a.map((v) => v.toString());
 const bMap = b.map((v) => v + "!");
 
 // 정답: map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): U[];
+
+// filter 타입 직접 만들어보기
+const aFilter = a.filter((v) => v % 2 == 0);
+
+const c: Arr<number | string> = [1, "2", 3, "4", 5];
+const cFilterFunc = (item: number | string): item is string =>
+  typeof item === "string";
+const cFilterResult = c.filter<string>(cFilterFunc);
+const cFilterResult2 = c.filter((v): v is number => typeof v === "number");
+
+// 정답:     filter<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S[];
+// filter(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): T[];
